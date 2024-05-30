6f35c3b28a2f066d67786f593826e6c9c75c5703
Anmol Tyagi <anmoltyagi908@gmail.com>
2024-05-31 00:03:13 +0530

    final changes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const contactRoutes = require('./routes/contactRoutes');
-const sequelize = require('./config/database');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const contactRoutes = require("./routes/contactRoutes");
+const sequelize = require("./config/database");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/api', contactRoutes);
+app.use("/api", contactRoutes);
+app.use(cors());
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   try {
     await sequelize.authenticate();
-    console.log('Connection to the database has been established successfully.');
+    console.log(
+      "Connection to the database has been established successfully."
+    );
     await sequelize.sync({ alter: true });
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 });
